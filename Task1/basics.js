7bbdafbc7649b030a64723bbe046e9984d0a4137
koushikkolli <31841404+koushikkolli@users.noreply.github.com>
2020-06-28 22:57:30 +0530

    Create basics.js

---
+++ Task1/basics.js
@@ -0,0 +1,48 @@
+// Declaring empty variables
+var a;
+var b;
+var c;
+var d;
+
+//Getting value of my variable
+var myvar = 1
+console.log(myvar)
+
+// Declaring variables in multiple lines
+var firstName;
+var lastName;
+var maritalStatus;
+var country;
+var age;
+
+// Declaring variables in single line
+var firstName1, lastName1, maritalStatus1, country1, age1;
+
+// Declaring and storing values of different datatypes
+var strVariable = "45";
+var intVariable = true;
+var undefinedVariable = undefined;
+var nullVariable = null;
+
+// Parsing the string in different ways
+var parseIntVariable = parseInt(strVariable);
+var numberVariable = Number(strVariable);
+var plusVariable = +strVariable;
+
+// Statements which result in Boolean values
+
+console.log("true" === true);
+console.log("CAPITAL" === "capital");
+console.log(12 === 12);
+console.log(12 > 10);
+console.log(12 <= 20);
+console.log((5 > 2) && (2 <=1));
+console.log((34 >= 12) || (9 < 2));
+
+
+
+
+
+
+
+
